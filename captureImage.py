6e6a2f3757dd63933233b2cb2052c2cfc9f8bb72
Eran Gross <you@example.com>
2024-01-04 19:41:02 +0200

    Imorving capture image

--- captureImage.py
+++ captureImage.py
@@ -1,6 +1,8 @@
 import cv2
+import os
+from datetime import datetime
 
-def capture_image():
+def capture_image(output_dir):
     cam = cv2.VideoCapture(0)
 
     ret, frame = cam.read()
@@ -8,9 +10,18 @@ def capture_image():
         print("Failed to grab frame")
         return
 
-    img_name = "opencv_frame.png"
-    cv2.imwrite(img_name, frame)
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Generate a unique filename with the current date and time
+    timestamp = datetime.now().strftime("%d%m%Y_%H%M%S")
+    img_name = f"{timestamp}.png"
+
+    # Save the image in the output directory
+    cv2.imwrite(os.path.join(output_dir, img_name), frame)
 
     cam.release()
 
-capture_image()
+# Specify the output directory when calling the function
+capture_image("/home/erangross/capture")
